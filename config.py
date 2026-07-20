4d5fabe1090f8a230cf82b74ca4efd2012ddfa20
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-20 14:36:14 +0530

    Update API_URL to new endpoint

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "6405089004:AAFnrAPvejPlOsx9CMgrJjh0JFpcib1oy5w"
 
-API_URL = "https://web-production-76bd6.up.railway.app/api/shorten"
+API_URL = "web-production-ec294.up.railway.app/api/shorten"
