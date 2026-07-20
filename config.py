cb737817675c552b4d5ad1d89aee840d52d7e08c
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-20 16:31:08 +0530

    Integrate dotenv for environment variable management
    Added dotenv support for environment variables and loaded .env file.

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 BOT_TOKEN = "6405089004:AAFnrAPvejPlOsx9CMgrJjh0JFpcib1oy5w"
 
 API_URL = "https://web-production-ec294.up.railway.app/api/shorten"
-
-MAX_LINKS = 40
