35b2a124dd2eece3a8abf9de6b4c6489cd862a98
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-22 11:21:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ load_dotenv()
 BOT_TOKEN = "6405089004:AAFnrAPvejPlOsx9CMgrJjh0JFpcib1oy5w"
 
 API_URL = "https://web-production-ec294.up.railway.app/api/shorten"
+
+BACKUP_CHANNEL_ID = -1001234567890 
