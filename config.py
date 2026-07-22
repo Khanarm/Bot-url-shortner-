3cf577eb54657e15e6282bee1c599a625603cec1
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-22 11:30:30 +0530

    Update BACKUP_CHANNEL_ID in config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ BOT_TOKEN = "6405089004:AAFnrAPvejPlOsx9CMgrJjh0JFpcib1oy5w"
 
 API_URL = "https://web-production-ec294.up.railway.app/api/shorten"
 
-BACKUP_CHANNEL_ID = -1001234567890 
+BACKUP_CHANNEL_ID = -1004468880790
